171e9c71d07f87a6cf5f0a9c7e0e315fabf70ccb
ronniebm <ronnie.coding@gmail.com>
2020-05-18 15:26:35 -0500

    uploading task 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+raise_exception_msg: a function that raises a Name exception with a msg.
+
+return: nothing.
+"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
