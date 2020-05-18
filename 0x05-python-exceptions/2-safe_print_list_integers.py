4b9416ef39a60dbaa9c3976324ee8e099d489a2a
ronniebm <ronnie.coding@gmail.com>
2020-05-18 11:27:43 -0500

    fixing except task2 with pep8 done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -16,7 +16,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 counter += 1
-        except (TypeError):
+        except TypeError:
             break
     print()
     return counter
