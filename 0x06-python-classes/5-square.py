e9a5ef11b467b66118cb8673501822bf5cd1f8f8
ronniebm <ronnie.coding@gmail.com>
2020-05-19 17:27:20 -0500

    uploading task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+# Defining class Square
+class Square:
+
+    # initializing variables
+    def __init__(self, size=0):
+        self.__size = size
+
+    # size property
+    @property
+    def size(self):
+        return self.__size
+
+    # setter property
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    # area calculation method
+    def area(self):
+        return self.__size ** 2
+
+    # size print method
+    def my_print(self):
+        size = self.__size
+
+        if size == 0:
+            print()
+
+        for row in range(size):
+            print('#' * size)
