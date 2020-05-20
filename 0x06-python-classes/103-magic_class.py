41d6db5d532c36571daebe5fce775d74d88b693b
ronniebm <ronnie.coding@gmail.com>
2020-05-19 20:56:25 -0500

    fixing task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ class MagicClass:
     '''defining class: MagicClass'''
 
     # initializing method
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
