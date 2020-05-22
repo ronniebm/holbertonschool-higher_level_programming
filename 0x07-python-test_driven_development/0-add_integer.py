cf9ba47c405da3a8c1d0b727776fe0260ec3e0b7
ronniebm <ronnie.coding@gmail.com>
2020-05-22 14:33:29 -0500

    uploading task0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''0-add_integer.py module'''
+
+
+def add_integer(a, b=98):
+        """
+        this function adds two (2) numbers.
+
+        Arguments:
+        ----------
+           a {int} -- [1st value]
+           b {int} -- [2nd value] (default: {98})
+
+        Raises:
+        -------
+           TypeError: [a and b must be int/float type only]
+
+        Returns:
+        --------
+          a value {int} -- [sum of two numbers]
+
+        """
+
+        msg = [
+                'a must be an integer',
+                'b must be an integer'
+        ]
+
+        if type(a) is not int and type(a) is not float:
+                raise TypeError(msg[0])
+
+        elif type(b) is not int and type(b) is not float:
+                raise TypeError(msg[1])
+
+        return (int(a) + int(b))
