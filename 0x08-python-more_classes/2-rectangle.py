681b97f76a084267fff8e4209ddf98d8cf561a1d
ronniebm <ronnie.coding@gmail.com>
2020-05-25 19:05:00 -0500

    fixing

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
     # width property setter.
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
@@ -49,10 +49,10 @@ class Rectangle:
     # height property setter.
     @height.setter
     def height(self, value):
-        if type(value) is not int:
-            raise TypeError('heigth must be an integer')
+        if isinstance(value, int):
+            raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('heigth must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     # Rectangle's area method.
