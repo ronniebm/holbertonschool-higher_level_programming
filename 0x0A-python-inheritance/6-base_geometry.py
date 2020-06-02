c864655825bacf6a7a21f8f3fccc140c8b83aeb3
ronniebm <ronnie.coding@gmail.com>
2020-06-02 15:40:49 -0500

    uploading task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""5-base_geometry.py module"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry empty class.
+
+    Raises:
+    -------
+    Exception: area(self) method not implemented.
+
+    Methods:
+    -------
+    area(self) -- [not implemented]
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
