fe7e2f226ec1256ef77422a57d3865c89f841b37
ronniebm <ronnie.coding@gmail.com>
2020-06-02 18:41:00 -0500

    fixing method docstric

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,4 +11,7 @@ class MyList(list):
         list  {class} -- [class 'list']
     """
     def print_sorted(self):
+        """
+        print_sorted method.
+        """
         print(sorted(self))
