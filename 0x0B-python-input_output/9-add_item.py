2a35da5904211809c7f0dedb7f139a2392fd5490
ronniebm <ronnie.coding@gmail.com>
2020-06-03 18:11:15 -0500

    uploading task9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,16 +4,18 @@
 from sys import argv
 from os import path
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-filename = 'add_item.json'
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
+    filename = 'add_item.json'
 
-if path.isfile(filename):
-    new_list = load_from_json_file(filename)
-else:
-    new_list = []
+    if path.isfile(filename):
+        new_list = load_from_json_file(filename)
+    else:
+        new_list = []
 
-for i in range(1, len(argv)):
-    new_list.append(argv[i])
+    for i in range(1, len(argv)):
+        new_list.append(argv[i])
 
-save_to_json_file(new_list, filename)
+    save_to_json_file(new_list, filename)
