5ba55643a8d742d6f70903cf626e79d5265b0b03
ronniebm <ronnie.coding@gmail.com>
2020-06-03 17:59:16 -0500

    uploading task9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""9-add_item.py script"""
+
+from sys import argv
+from os import path
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+filename = 'add_item.json'
+
+if path.isfile(filename):
+    new_list = load_from_json_file(filename)
+else:
+    new_list = []
+
+for i in range(1, len(argv)):
+    new_list.append(argv[i])
+
+save_to_json_file(new_list, filename)
