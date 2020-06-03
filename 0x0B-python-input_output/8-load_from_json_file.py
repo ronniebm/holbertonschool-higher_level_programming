1d6466e347ded8c8bb31f6f08d50e8669614a673
ronniebm <ronnie.coding@gmail.com>
2020-06-03 17:15:53 -0500

    uploading task8

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""8-load_from_json_file.py module"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file: creates an
+    Object from a "JSON file"
+
+    Arguments:
+    ---------
+    filename [str] -- name of the file.
+
+    Returns:
+    --------
+    object: a JSON object
+    """
+    with open(filename, encoding='utf-8') as file:
+        return json.loads(file.read())
