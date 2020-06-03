2a1f64adcb3f6c5516b6917d771cc2cc2f12e187
ronniebm <ronnie.coding@gmail.com>
2020-06-03 14:16:18 -0500

    uploading task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     """
     read_file: reads the given file.
     """
-    with open("./my_file_0.txt", mode='r', encoding='utf8') as fileObj:
+    with open(filename, mode='r', encoding='utf8') as fileObj:
         for line in fileObj:
             print(line, end="")
