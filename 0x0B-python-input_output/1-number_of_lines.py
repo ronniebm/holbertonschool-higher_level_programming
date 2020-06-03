beea52e542f9f8b89de2e33b8b4cade300150452
ronniebm <ronnie.coding@gmail.com>
2020-06-03 14:17:08 -0500

    uploading task1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -7,7 +7,7 @@ def number_of_lines(filename=""):
     number_of_lines: Todo.
     """
     counter = 0
-    with open("./my_file_0.txt", mode='r', encoding='utf8') as fileObj:
+    with open(filename, mode='r', encoding='utf8') as fileObj:
         for line in fileObj:
             counter += 1
         return (counter)
