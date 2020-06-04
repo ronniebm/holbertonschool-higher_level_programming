60eb2209b73ede94ea00cdc13bfa6cde49aa0899
ronniebm <ronnie.coding@gmail.com>
2020-06-03 19:39:21 -0500

    uploading task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""10-class_to_json.py module"""
+
+
+def class_to_json(obj):
+    """
+    class_to_json: Returns the dictionary
+    represntation of a simple data structure.
+    """
+    return (obj.__dict__)
