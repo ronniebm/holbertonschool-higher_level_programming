36202934510598c103616d693fc079d3b6074f37
ronniebm <ronnie.coding@gmail.com>
2020-06-03 19:33:36 -0500

    fixing

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 """9-add_item.py script"""
 
-import sys
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-my_list = []
 try:
-    my_list = load_from_json_file(filename)
+    items = load_from_json_file('add_item.json')
 except:
-    pass
-finally:
-    for idx in range(1, len(sys.argv)):
-        my_list.append(sys.argv[idx])
-    save_to_json_file(my_list, filename)
-    
+    items = []
+
+save_to_json_file(items + argv[1:], 'add_item.json')
