f48c9b2c1d686e63cf2021dc4cd5e44f3303f713
ronniebm <ronnie.coding@gmail.com>
2020-06-06 13:29:28 -0500

    uploading test cases task 1,2

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,40 +2,24 @@
 import unittest
 from models.base import Base
 
-class TestClassBase(unittest.TestCase):
-    """
-        unit testing class
-    """
-    def test_a0(self):
+class Test_task0(unittest.TestCase):
+    """unit testing for task0."""
+
+    def test_a0_instances_creation(self):
         """no args, obj#1"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base().id, 2)
+        self.assertEqual(Base('Holberton').id, 'Holberton')
+        self.assertEqual(Base((2,3,4,5,6)).id, (2,3,4,5,6))
+        self.assertEqual(Base().id, 3)
+        self.assertEqual(Base().id, 4)
 
-    def test_a1(self):
-        """no args, obj#2"""
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-
-    def test_a2(self):
-        """arg is a string, obj#3"""
-        b3 = Base('Holberton')
-        self.assertEqual(b3.id, 'Holberton')
-
-    def test_a3(self):
-        """arg is a tuple, obj#4"""
-        b4 = Base((2,3,4,5,6))
-        self.assertEqual(b4.id, (2,3,4,5,6))
-
-    def test_a4(self):
-        """two obj in one test case, with no args"""
-        b5 = Base()
-        b6 = Base()
-        self.assertEqual(b5.id, 3)
-        self.assertEqual(b6.id, 4)
-
-    def test_a5(self):
-        """more than one arg"""
+    def test_a1_two_args(self):
+        """testing TypeError"""
         with self.assertRaises(TypeError):
             Base(2, 'b')
-            print(Base)
 
+    def test_a2_class_atrib(self):
+        """testing private class attribute"""
+        with self.assertRaises(AttributeError):
+            print(Base(13).__nb_objects)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,26 +3,61 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class TestClassRectangle(unittest.TestCase):
-    """
-        unit testing class Rectangle
-    """
+class Test_task2(unittest.TestCase):
+    """unit testing for task2."""
+
     def test_a0(self):
-        """creating obj#1 withoud id"""
+        """Testing instances creation"""
+        self.assertEqual(Rectangle(10, 2).id, 1)
+        self.assertEqual(Rectangle(4, 2, 3).id, 2)
+        self.assertEqual(Rectangle(2, 3, 4, 5).id, 3)
+        self.assertEqual(Rectangle(2, 3, 4, 5, 11).id, 11)
+        self.assertEqual(Rectangle(2, 1, 3, 4, 'Holb').id, 'Holb')
+        self.assertEqual(Rectangle(2, 1, 2, 4, 'School').id, 'School')
+
+        # Instantiation with no args.
+        with self.assertRaises(TypeError):
+            Rectangle()
+
+        # Instantiation missing one arg.
+        with self.assertRaises(TypeError):
+            Rectangle(1)
+
+    def test_a1_private_width(self):
+        """private width"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(5, 5, 0, 0, 1).__width)
+
+    def test_a2_private_height(self):
+        """private height"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(5, 5, 0, 0, 1).__height)
+
+    def test_a3_private_x(self):
+        """private x"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(5, 5, 0, 0, 1).__x)
+
+    def test_a4_private_y(self):
+        """private y"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(5, 5, 0, 0, 1).__y)
+    
+    def test_a5_setter_and_getters(self):
+        """getters for all attributes"""
+        r = Rectangle(5, 7, 6, 5, 20)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 7)
+        self.assertEqual(r.x, 6)
+        self.assertEqual(r.y, 5)
+        self.assertEqual(r.id, 20)
+        
+        
+
+""" class Test_Rectangle_width(unittest.TestCase):
+    '''
+    Unittests for Rectangle's width.
+    '''
+    def test_t0(self):
         b1 = Rectangle(2, 3, 0, 0)
-        self.assertEqual(b1.id, 1)
-
-    def test_a1(self):
-        """no args, obj#2"""
-        b2 = Rectangle(4, 5, 2, 2)
-        self.assertTrue(b2.id, 2)
-
-    def test_a2(self):
-        """arg is a string, obj#3"""
-        b3 = Rectangle(2, 1, 3, 4, 'Holberton')
-        self.assertEqual(b3.id, 'Holberton')
-
-    def test_a3(self):
-        """arg is a string, obj#3"""
-        b3 = Rectangle(2, 1, 2, 4, 'Holberton')
-        self.assertEqual(b3.id, 'Holberton')
+ """
