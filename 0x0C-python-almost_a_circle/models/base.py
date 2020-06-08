69725bc697bf04f8678cb3e5fb515b2c844ced78
ronniebm <ronnie.coding@gmail.com>
2020-06-08 14:22:41 -0500

    first commit task 17 working

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,12 +29,12 @@ class Base():
         """
         save_to_file class method.
         --------------------------
-        writes the JSON string representation of 
+        writes the JSON string representation of
         list_objs to a file.
 
         Arguments:
         ---------
-        list_objs [list] -- a list of instances who 
+        list_objs [list] -- a list of instances who
         inherits of Base.
         """
         file = cls.__name__ + '.json'
@@ -47,3 +47,19 @@ class Base():
                 for i in range(len(list_objs)):
                     l_dict.append(list_objs[i].to_dictionary())
                 myfile.write(json.dumps(l_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        from_json_strinc class method.
+        -----------------------------
+
+        Arguments:
+        ----------
+        json_string [str] -- is a string representing a
+        list of dictionaries.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        else:
+            return json.loads(json_string)
