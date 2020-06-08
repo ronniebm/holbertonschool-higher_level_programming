789f0826060522db30a2617938cb82763dc65572
ronniebm <ronnie.coding@gmail.com>
2020-06-08 12:23:06 -0500

    first commit task 15 working

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ base.py module """
+import json
 
 
 class Base():
@@ -13,3 +14,12 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to_json_string static method"""
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
