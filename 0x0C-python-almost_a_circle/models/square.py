138cba150b1b6868d68a8fc0b9d00913c6bf0a79
ronniebm <ronnie.coding@gmail.com>
2020-06-08 11:49:09 -0500

    first commit task 12 working

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,3 +37,25 @@ class Square(Rectangle):
         y = self.y
         _id = self.id
         return("[Square] ({0}) {1}/{2} - {3}".format(_id, x, y, s))
+
+    def update(self, *args, **kwargs):
+        """update method"""
+        _len = len(args)
+        if not args:
+            if kwargs.get('id') is not None:
+                self.id = kwargs.get('id')
+            if kwargs.get('size') is not None:
+                self.size = kwargs.get('size')
+            if kwargs.get('x') is not None:
+                self.x = kwargs.get('x')
+            if kwargs.get('y') is not None:
+                self.y = kwargs.get('y')
+        else:
+            if _len >= 1 and args[0] is not None:
+                self.id = args[0]
+            if _len >= 2 and args[1] is not None:
+                self.size = args[1]
+            if _len >= 3 and args[2] is not None:
+                self.x = args[2]
+            if _len >= 4 and args[3] is not None:
+                self.y = args[3]
