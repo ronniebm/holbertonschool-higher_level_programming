6ec3e4b26bb937da36f712c575c52075bc094315
ronniebm <ronnie.coding@gmail.com>
2020-06-09 15:03:21 -0500

    fixing case

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -112,11 +112,12 @@ class Test_task18(unittest.TestCase):
 
     def test_d3_create_zzz(self):
         """This function tests the create func"""
+        Base._Base__nb_objects = 0
         r1 = Rectangle(3, 5, 1)
         r1_dictionary = r1.to_dictionary()
         r2 = Rectangle.create(**r1_dictionary)
-        self.assertEqual(str(r1), "[Rectangle] (6) 1/0 - 3/5")
-        self.assertEqual(str(r2), "[Rectangle] (7) 1/0 - 3/5")
+        self.assertEqual(str(r1), "[Rectangle] (1) 1/0 - 3/5")
+        self.assertEqual(str(r2), "[Rectangle] (1) 1/0 - 3/5")
         self.assertEqual(r1 is r2, False)
         self.assertEqual(r1 == r2, False)
 
