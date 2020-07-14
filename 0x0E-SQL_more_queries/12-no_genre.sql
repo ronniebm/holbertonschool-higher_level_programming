19283c9c2469e6deabe4425d4a0cb9d68a9cff06
ronniebm <ronnie.coding@gmail.com>
2020-07-14 17:32:41 -0500

    uploading task#12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- it lists all shows without a 
+-- linked gender.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
