919e873734df774ec61c938b8a0f3610e90d128d
ronniebm <ronnie.coding@gmail.com>
2020-07-14 12:08:27 -0500

    uploading task#6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- it creates the database called 'hbtn_0d_usa' 
+-- and the 'states' table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS 
+states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
