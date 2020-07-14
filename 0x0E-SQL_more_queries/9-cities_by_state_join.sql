f5665ddab0034d63e460c9fe84c42e6154261155
ronniebm <ronnie.coding@gmail.com>
2020-07-14 12:13:53 -0500

    uploading task#8

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- it lists all cities contained in 
+-- 'hbtn_0d_usa' database.
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER by cities.id ASC;
