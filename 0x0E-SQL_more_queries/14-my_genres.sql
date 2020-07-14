f76d9503e695b6114bfd69aebb428ea5071c0cc5
ronniebm <ronnie.coding@gmail.com>
2020-07-14 17:40:31 -0500

    uploading task#14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- all genders of the Dexter's show.
+-- lists all genres of Dexter's show.
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
