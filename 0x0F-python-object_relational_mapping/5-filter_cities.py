d4b3fc64140506736512228825a9fa1ce6dbf267
ronniebm <ronnie.coding@gmail.com>
2020-08-08 14:37:41 -0500

    first commit task#5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+This script lists all cities from database 'hbtn_0e_4_usa'
+'''
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    # declaring arguments passed, with query.
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    name = argv[4]
+    query = """SELECT cities.name
+FROM states INNER JOIN cities ON states.id = cities.state_id
+WHERE states.name=%s ORDER BY cities.id ASC"""
+
+    # creating connection to the database.
+    db_connection = MySQLdb.connect(host="localhost", port=3306, user=user,
+                                    passwd=passwd, db=db, charset="utf8")
+
+    # Making a cursor Object for query execution.
+    cursor = db_connection.cursor()
+
+    # Executing query.
+    cursor.execute(query, (name,))
+    query_rows = cursor.fetchall()
+
+    # Printing DATABASE
+    for i in range(len(query_rows)-1):
+        print(str(query_rows[i]).split("'")[1], end=", ")
+    print(str(query_rows[i+1]).split("'")[1])
+
+    cursor.close()
+    db_connection.close()
