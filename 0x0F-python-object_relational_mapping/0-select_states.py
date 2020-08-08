6304643b33507acf2832736352237acc75b97dec
ronniebm <ronnie.coding@gmail.com>
2020-08-07 20:16:25 -0500

    first commit task#0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''This script displays states by ID in ascending order'''
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    # declaring arguments passed.
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    # creating connection to the database.
+    db_connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3], charset="utf8")
+
+    # Making a cursor Object for query execution.
+    cursor = db_connection.cursor()
+
+    # Executing query.
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+
+    # Printing DATABASE
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    db_connection.close()
