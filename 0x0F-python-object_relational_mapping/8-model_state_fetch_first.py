e988161dc01dbdad355b2357f852e1fd1ba7835e
ronniebm <ronnie.coding@gmail.com>
2020-08-09 10:09:23 -0500

    trying first commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+A script that prints the first State
+object from the database 'hbtn_0e_6_usa'.
+'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # Creating an instance.
+    session = Session(engine)
+
+    # reading first State object.
+    first = session.query(State).order_by(State.id).first()
+
+    # printing from database.
+    try:
+        print("{}: {}".format(first.id, first.name))
+    except:
+        print("Nothing")
+
+    session.close()
