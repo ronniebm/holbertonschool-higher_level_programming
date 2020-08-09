b1ae1a8e20483da55fb234d4edf2a80a695c8261
ronniebm <ronnie.coding@gmail.com>
2020-08-09 09:53:29 -0500

    fixing issues

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,7 @@ from the database hbtn_0e_6_usa.
 
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 
