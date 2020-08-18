399534a38eb065301b22fdab0898c9d1f93ec70a
ronniebm <ronnie.coding@gmail.com>
2020-08-18 12:11:18 -0500

    fixing comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,7 @@
 # shows body size of a given URL.
 
 # [-s]: silent mode   => don't show any error message.
+# [-I]: include       => include http response headers!
+#                        server name, cookies, date, etc.
+
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
