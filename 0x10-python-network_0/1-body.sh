1d54624ff908c508f19d760d4edea1dd7f742485
ronniebm <ronnie.coding@gmail.com>
2020-08-18 12:23:47 -0500

    fixing only 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # takes in a URL, send a get request, and show response's body.
-
-# [-s]: silent mode   => don't show any error message.
-# [-f]: Fail silently => return error 22, instead a 'document failed' from http server!
-# [-L]: Location      => if server report 'moved page' this option will make curl redo
-#                        the request on the new place.
 curl -sfL "$1"
