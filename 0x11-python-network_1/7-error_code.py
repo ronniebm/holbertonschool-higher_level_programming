2da7934e843c1c2e153a943a214320bb9bd38d99
ronniebm <ronnie.coding@gmail.com>
2020-08-24 21:09:12 +0000

    fixin if condition

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ import sys
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
-        print(response.text)
-    else:
         print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
