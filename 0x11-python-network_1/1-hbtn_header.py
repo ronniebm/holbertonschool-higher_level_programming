1a858cba968773b9619ba29f1fa3ad56bb57d2db
ronniebm <ronnie.coding@gmail.com>
2020-08-24 17:21:58 +0000

    trying getheader method

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """ Default doctstring. """
-import urllib.request
+import urllib.request as url
 import sys
 
 if __name__ == "__main__":
-    my_url = sys.argv[1]
-    with urllib.request.urlopen('{}'.format(my_url)) as response:
-        html = response.headers
-
-        print(html._headers[11][1])
+    with url.urlopen(sys.argv[1]) as header:
+        print(header.getheader('X-Request-Id'))
