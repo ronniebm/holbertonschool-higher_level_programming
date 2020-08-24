19df4dfa768859fcd3e6b6d66a6b8df95e712098
ronniebm <ronnie.coding@gmail.com>
2020-08-24 14:49:12 +0000

    testing vscode local docker connection

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 """ Default doctstring. """
-from requests import urllib3
 
-req = urllib3.request.RequestMethods('http://www.voidspace.org.uk')
-with urllib.request.urlopen(req) as response:
-   the_page = response.read()
+print('testing a visual studio container connection')
