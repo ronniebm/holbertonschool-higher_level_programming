f09d3cfee2a97fd59ebcb707b5fb50f1caa2daa5
ronniebm <ronnie.coding@gmail.com>
2020-08-24 20:21:01 +0000

    fixing

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    if response:
+    if response.status_code >= 400:
         print(response.text)
     else:
         print("Error code: {}".format(response.status_code))
