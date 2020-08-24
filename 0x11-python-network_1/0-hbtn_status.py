4d8637d67537d747ec29b840b008f0942fd04154
ronniebm <ronnie.coding@gmail.com>
2020-08-24 16:20:54 +0000

    uploading task#0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """ Default doctstring. """
+import urllib.request
 
-print('testing a visual studio container connection')
+if __name__ == "__main__":
+    my_url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(my_url) as response:
+        html = response.read()
+
+    print("""Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".format(type(html), html, html.decode("utf-8")))
