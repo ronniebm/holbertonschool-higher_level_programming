ed90f6453657d88e673c54e4caca2484736acffb
ronniebm <ronnie.coding@gmail.com>
2020-09-15 12:33:54 -0500

    fixing conditional

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,6 @@ if (process.argv.slice(2).length === 0) {
   console.log('No argument');
 } else if (process.argv.slice(2).length === 1) {
   console.log('Argument found');
-} else if (process.argv.slice(2).length === 2) {
+} else if (process.argv.slice(2).length > 1) {
   console.log('Arguments found');
 }
