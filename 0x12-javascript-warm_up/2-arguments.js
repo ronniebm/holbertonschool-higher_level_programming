67ac11dd076e97097c88298f3aef6930ed85002d
ronniebm <ronnie.coding@gmail.com>
2020-09-15 10:37:57 -0500

    fixing my syntax

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (Object.keys(process.argv.slice(2)).length == 0) {
+if (process.argv.slice(2).length == 0) {
   console.log('No argument');
-} else if (Object.keys(process.argv.slice(2)).length == 1) {
+} else if (process.argv.slice(2).length == 1) {
   console.log('Argument found');
-} else if (Object.keys(process.argv.slice(2)).length == 2) {
+} else if (process.argv.slice(2).length == 2) {
   console.log('Arguments found');
 }
