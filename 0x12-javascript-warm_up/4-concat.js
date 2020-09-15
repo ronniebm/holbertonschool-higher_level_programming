a6fa7dea7d2d45f1ddfcd6f03210a265cc8700df
ronniebm <ronnie.coding@gmail.com>
2020-09-15 10:53:14 -0500

    first commit task#4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.slice(2).length == 0) {
+  console.log('undefined is undefined');
+} else if (process.argv.slice(2).length == 1) {
+  console.log(process.argv.slice(2)[0] + ' is undefined');
+} else if (process.argv.slice(2).length == 2) {
+  console.log(process.argv.slice(2)[0] + ' is ' + process.argv.slice(2)[1]);
+}
