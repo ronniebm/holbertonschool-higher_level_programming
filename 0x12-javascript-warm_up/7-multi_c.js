8a7073fd1614997b31d1ff247e56b5097c133575
ronniebm <ronnie.coding@gmail.com>
2020-09-15 13:04:38 -0500

    fixing semistandard syntax

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-if (process.argv.slice(2).length == 0) {
-    console.log('Missing number of occurrences');
-} else if (process.argv.slice(2).length == 1) {
-    if (isNaN(parseInt(process.argv.slice(2)[0])) == false) {
-      for (i = 0; i < parseInt(process.argv.slice(2)[0]); i++) {
-        console.log("C is fun");
-      }
+if (process.argv.slice(2).length === 0) {
+  console.log('Missing number of occurrences');
+} else if (process.argv.slice(2).length === 1) {
+  if (isNaN(parseInt(process.argv.slice(2)[0])) === false) {
+    let i;
+    for (i = 0; i < parseInt(process.argv.slice(2)[0]); i++) {
+      console.log('C is fun');
     }
+  }
 }
