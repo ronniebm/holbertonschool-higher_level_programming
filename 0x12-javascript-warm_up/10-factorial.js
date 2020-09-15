a55de0628c363ee0124e31398a1ba0014a6bc768
ronniebm <ronnie.coding@gmail.com>
2020-09-15 13:19:19 -0500

    first try task#10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial (a) {
+  const num = parseInt(a);
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  if (a === 0) {
+    return 1;
+  }
+
+  return a * factorial(num - 1);
+}
+
+console.log(factorial(process.argv[2]));
