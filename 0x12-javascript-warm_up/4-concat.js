a5394a180fd5c8033d76d816dec4debc644e8e2c
ronniebm <ronnie.coding@gmail.com>
2020-09-15 12:42:38 -0500

    fixing semistandard syntax

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.slice(2).length == 0) {
+if (process.argv.slice(2).length === 0) {
   console.log('undefined is undefined');
-} else if (process.argv.slice(2).length == 1) {
+} else if (process.argv.slice(2).length === 1) {
   console.log(process.argv.slice(2)[0] + ' is undefined');
-} else if (process.argv.slice(2).length == 2) {
+} else if (process.argv.slice(2).length === 2) {
   console.log(process.argv.slice(2)[0] + ' is ' + process.argv.slice(2)[1]);
 }
