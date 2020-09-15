4e5abfbe21c81dc532409722dfd659e73d7fe8fc
ronniebm <ronnie.coding@gmail.com>
2020-09-15 12:32:34 -0500

    fixing

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv.slice(2).length == 0) {
+if (process.argv.slice(2).length === 0) {
   console.log('No argument');
-} else if (process.argv.slice(2).length == 1) {
+} else if (process.argv.slice(2).length === 1) {
   console.log(process.argv.slice(2)[0]);
 }
