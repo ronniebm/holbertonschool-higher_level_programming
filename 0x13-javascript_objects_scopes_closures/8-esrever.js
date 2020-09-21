154b1794db04f186a313bbde6bf9ae977a57218e
ronniebm <ronnie.coding@gmail.com>
2020-09-21 12:43:08 -0500

    testing task#8 outputs ok

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/**
+ * A function that returns the the reversed
+ * version of a list.
+ * -----------------------------------------
+ */
+
+exports.esrever = function (list) {
+  const len = (list.length) - 1;
+  const newList = [];
+
+  for (let i = len; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+
+  return (newList);
+};
