0962aac08b130075065b5aa681b9a2b1aa3c9e0c
ronniebm <ronnie.coding@gmail.com>
2020-09-23 15:55:33 -0500

    fixing

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+/* A script that prints the number of
+ * movies where the character “Wedge
+ * Antilles” is present.
+ */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('Invalid');
+  }
+});
