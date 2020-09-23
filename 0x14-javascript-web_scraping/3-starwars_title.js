b4cc31ff98bb75ba7b89d21fc05f02360af3fad5
ronniebm <ronnie.coding@gmail.com>
2020-09-23 15:49:33 -0500

    try task#3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/* A script that prints the title of
+ * a Star Wars movie where the episode
+ * number matches a given integer.
+ */
+
+const request = require('request');
+const argv = process.argv;
+const url = 'http://swapi.co/api/films/';
+
+request(url + argv[2], function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
