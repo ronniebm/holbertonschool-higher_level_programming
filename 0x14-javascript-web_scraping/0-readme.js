4b7fc2d288a8db61c15a8d1e6897f26287cf906a
ronniebm <ronnie.coding@gmail.com>
2020-09-23 15:43:00 -0500

    first try task#0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/* A script that Reads and prints the
+ *content of a file
+ */
+
+const argv = process.argv;
+const fs = require('fs');
+
+fs.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
