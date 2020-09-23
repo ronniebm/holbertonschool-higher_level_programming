db1715ff6aa646d9536f22e5f0ddb194a1056063
ronniebm <ronnie.coding@gmail.com>
2020-09-23 15:59:41 -0500

    fixing

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* A script that gets the contents of a
+ * webpage and stores it in a file.
+ */
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(file, body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
