55d4905e0cc0d6eb7a4f66202368a83ec5a8bcb2
ronniebm <ronnie.coding@gmail.com>
2020-09-24 14:31:30 -0500

    uploading task#5

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+/* a Javascript script that toggles the class
+ of the HTML tag HEADER to red (#FF0000) when
+ the user clicks on the tag DIV#toggle_header */
+
+$('div#add_item').click(function () {
+  $('.my_list').append('<li>Item</li>');
+});
