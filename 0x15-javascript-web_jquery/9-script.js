7f51b5d87e84446b85ccf40d493422e4dc882c05
ronniebm <ronnie.coding@gmail.com>
2020-09-24 15:32:34 -0500

    task#9 get from API ok

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+/* a Javascript script that fetches from
+https://fourtonfish.com/hellosalut/?lang=fr
+and displays the value of hello from that
+fetch in the HTML’s tag DIV#hello. */
+
+$.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (resp) {
+  $('#hello').text(resp.hello);
+});
