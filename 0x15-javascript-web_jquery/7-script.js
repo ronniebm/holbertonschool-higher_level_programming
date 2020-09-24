55d39b8cdb64bda455a35079afd653dfed8378fb
ronniebm <ronnie.coding@gmail.com>
2020-09-24 15:12:44 -0500

    task#7 GET from API working

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+/* a Javascript script that fetches and replaces
+the name of this URL:
+https://swapi-api.hbtn.io/api/people/5/?format=json */
+
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (resp) {
+  $('#character').text(resp.name);
+});
